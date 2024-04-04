4ce6cc42a738c14e2aa4dfd07bef2d2a484d36aa
niño tesoro <na.tesoro@gmail.com>
2024-04-04 01:56:16 +0000

    20240404 Niño: Implemented the requirements. see below:
    Create a function named calculate() in mean_var_std.py that uses Numpy to output the mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix.
    The input of the function should be a list containing 9 digits. The function should convert the list into a 3 x 3 Numpy array, and then return a dictionary containing the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix.
    
    The returned dictionary should follow this format:
    {
      'mean': [axis1, axis2, flattened],
      'variance': [axis1, axis2, flattened],
      'standard deviation': [axis1, axis2, flattened],
      'max': [axis1, axis2, flattened],
      'min': [axis1, axis2, flattened],
      'sum': [axis1, axis2, flattened]
    }
    If a list containing less than 9 elements is passed into the function, it should raise a ValueError exception with the message: "List must contain nine numbers." The values in the returned dictionary should be lists and not Numpy arrays.
    
    For example, calculate([0,1,2,3,4,5,6,7,8]) should return:
    {
      'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
      'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
      'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
      'max': [[6, 7, 8], [2, 5, 8], 8],
      'min': [[0, 1, 2], [0, 3, 6], 0],
      'sum': [[9, 12, 15], [3, 12, 21], 36]
    }

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,39 @@ import numpy as np
 
 def calculate(list):
 
+    if (len(list)<9):
+        raise ValueError('List must contain nine numbers.')
+    
+    a2=np.array(list).reshape(3,3)
 
+    mean1=np.mean(a2, axis=0).tolist()
+    mean2=np.mean(a2, axis=1).tolist()
+    mean3=np.mean([np.mean(mean1),np.mean(mean2)])
 
+    var1=np.var(a2, axis=0).tolist()
+    var2=np.var(a2, axis=1).tolist()
+    var3=np.var(a2)
 
+    dev1=np.std(a2, axis=0).tolist()
+    dev2=np.std(a2, axis=1).tolist()
+    dev3=np.std(a2)
+
+    max1=np.max(a2, axis=0).tolist()
+    max2=np.max(a2, axis=1).tolist()
+
+    min1=np.min(a2, axis=0).tolist()
+    min2=np.min(a2, axis=1).tolist()
+    
+    #alternative method
+    sum1=[np.sum(a2[:,0]), np.sum(a2[:,1]), np.sum(a2[:,2])]
+    sum2=[np.sum(a2[0]), np.sum(a2[1]), np.sum(a2[2])]
+    
+    calculations={
+        "mean": [mean1, mean2, mean3],
+        "variance": [var1, var2, var3],
+        "standard deviation": [dev1, dev2, dev3],
+        "max": [max1, max2, np.max(a2)],
+        "min": [min1, min2, np.min(a2)],
+        "sum": [sum1, sum2, np.sum(a2)]
+    }
     return calculations
